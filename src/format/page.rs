3ba32452e0472e13821af6ecdb393ab004ca5a93
bert2 <shuairan@gmail.com>
2021-12-20 13:24:05 +0100

    reduce nesting

--- src/format/page.rs
+++ src/format/page.rs
@@ -40,30 +40,30 @@ impl<'a> Page<'a> {
         db: &'a [u8],
     ) -> impl Iterator<Item = Result<Page<'a>>> {
         if self.header.page_type == PageType::LeafTbl {
-            IterEither::left(iter::once(Ok(self)))
-        } else {
-            assert!(
-                self.header.page_type == PageType::IntrTbl,
-                "Cannot get leaf pages of {:?}",
-                self.header.page_type
-            );
-
-            let cell_ptrs_offset =
-                self.header.size() + if self.is_db_schema { DbHeader::SIZE } else { 0 };
-
-            let leaves = self.data[cell_ptrs_offset..]
-                .chunks_exact(2)
-                .take(self.header.number_of_cells.into())
-                .map(|bytes| usize::from(u16::from_be_bytes(bytes.try_into().unwrap())))
-                .map(move |cell_ptr| IntrTblCell::parse(&self.data[cell_ptr..]))
-                .map_ok_and_then(move |cell| Page::parse(cell.child_page, page_size, db))
-                .flat_map_ok_and_then(move |page| {
-                    Box::new(page.leaf_pages(page_size, db))
-                        as Box<dyn Iterator<Item = Result<Page<'a>>>>
-                });
-
-            IterEither::right(leaves)
+            return IterEither::left(iter::once(Ok(self)));
         }
+
+        assert!(
+            self.header.page_type == PageType::IntrTbl,
+            "Cannot get leaf pages of {:?}",
+            self.header.page_type
+        );
+
+        let cell_ptrs_offset =
+            self.header.size() + if self.is_db_schema { DbHeader::SIZE } else { 0 };
+
+        let leaves = self.data[cell_ptrs_offset..]
+            .chunks_exact(2)
+            .take(self.header.number_of_cells.into())
+            .map(|bytes| usize::from(u16::from_be_bytes(bytes.try_into().unwrap())))
+            .map(move |cell_ptr| IntrTblCell::parse(&self.data[cell_ptr..]))
+            .map_ok_and_then(move |cell| Page::parse(cell.child_page, page_size, db))
+            .flat_map_ok_and_then(move |page| {
+                Box::new(page.leaf_pages(page_size, db))
+                    as Box<dyn Iterator<Item = Result<Page<'a>>>>
+            });
+
+        IterEither::right(leaves)
     }
 
     pub fn cells(self) -> impl Iterator<Item = Result<LeafTblCell<'a>>> {
